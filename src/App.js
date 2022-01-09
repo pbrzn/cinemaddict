a6a796792610df0550ffb00e48db0d3d25348f13
Patrick Brennan <pbrennanmusic@gmail.com>
2022-01-08 19:39:09 -0500

    fix: Imported children components and added routes to each component in render().

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
+import NewUserForm from './components/NewUserForm';
+import Login from './components/Login';
+import Logout from './components/Logout';
+import Profile from './components/Profile';
+import ReviewsContainer from './components/containers/ReviewsContainer';
+// import Movies from './components/containers/MoviesContainer';
+import NavBar from './components/NavBar';
+
 import './App.css';
 
 class App extends Component {
-  render(){
+
+  render() {
     return (
-      <div>
+      <>
+        <NavBar />
         <Router>
-          <Route path="/home" component={<Home />} />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/register" element={<NewUserForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/reviews" element={<ReviewsContainer />} />
+          </Routes>
         </Router>
-      </div>
+      </>
     );
   }
 }
