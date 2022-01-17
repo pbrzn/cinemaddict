ec8864439788ef411877aee1d71af6f534770819
Patrick Brennan <pbrennanmusic@gmail.com>
2022-01-17 01:06:48 -0500

    fix: Uncommented reviewsActions for later use, deleted ReviewsLists for redundancy, and added to reviewsReducer

--- cinemaddict-frontend/src/components/actions/reviewsActions.js
+++ cinemaddict-frontend/src/components/actions/reviewsActions.js
@@ -1,37 +1,53 @@
-// const addReview = reviewObject => {
-//   dispatch({ type: 'START_ADD_REVIEW_POST_REQUEST' });
-//   fetch("http://localhost:3000/api/v1/reviews", {
-//     method: "POST",
-//     headers: {
-//       "Content-type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify(reviewObject)
-//   })
-//   .then(resp => resp.json())
-//   .then(data => dispatch({
-//     type: 'ADD_REVIEW',
-//     data
-//   })
-//
-// );
-//
-// const deleteReview = reviewId => {
-//   dispatch({ type: 'START_DELETE_REVIEW_REQUEST' });
-//   fetch(`http://localhost:3000/api/v1/reviews/${reviewId}`, {
-//     method: "DELETE",
-//     headers: {
-//       "Content-type": "application/json",
-//       "Accept": "application/json"
-//     },
-//     body: JSON.stringify(reviewId)
-//   })
-//   .then(resp => resp.json())
-//   .then(data => dispatch({
-//     type: 'DELETE_REVIEW',
-//     reviewId: reviewId
-//   }))
-// }
+export function addReview(reviewObject) => {
+  fetch("http://localhost:3000/api/v1/reviews", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json",
+      "Authorization": `Bearer ${localStorage.jwt}`
+    },
+    body: JSON.stringify(reviewObject)
+  })
+  .then(resp => resp.json())
+  .then(data => dispatch({
+    type: 'ADD_REVIEW',
+    data
+  })
+);
+
+export function editReview(reviewObject) => {
+  fetch("http://localhost:3000/api/v1/reviews", {
+    method: "PATCH",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json",
+      "Authorization": `Bearer ${localStorage.jwt}`
+    },
+    body: JSON.stringify(reviewObject)
+  })
+  .then(resp => resp.json())
+  .then(data => dispatch({
+    type: 'EDIT_REVIEW',
+    data
+  })
+);
+
+export function deleteReview(reviewId) {
+  fetch(`http://localhost:3000/api/v1/reviews/${reviewId}`, {
+    method: "DELETE",
+    headers: {
+      "Content-type": "application/json",
+      "Accept": "application/json",
+      "Authorization": `Bearer ${localStorage.jwt}`
+    },
+    body: JSON.stringify(reviewId)
+  })
+  .then(resp => resp.json())
+  .then(data => dispatch({
+    type: 'DELETE_REVIEW',
+    reviewId: reviewId
+  }))
+}
 //
 // const fetchreviews = () => {
 //   dispatch({ type: 'START_FETCHING_REVIEWS' });
--- cinemaddict-frontend/src/reducers/reviewsReducer.js
+++ cinemaddict-frontend/src/reducers/reviewsReducer.js
@@ -3,6 +3,10 @@ export default function reviewsReducer(state = [], action) {
     case 'ADD_REVIEW':
       return state.concat(action.review);
 
+    case 'EDIT_REVIEW':
+      state[action.review.id] = action.review;
+      return state;
+
     case 'DELETE_REVIEW':
       return state.filter(review => review.id !== action.reviewId);
 
