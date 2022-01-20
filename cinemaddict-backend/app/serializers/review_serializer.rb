e7bb617485201c4aa48c086eddddafa76647b1d3
Patrick Brennan <pbrennanmusic@gmail.com>
2022-01-20 13:23:18 -0500

    fix: Added movie_id and user_id to visible API data

--- cinemaddict-backend/app/serializers/review_serializer.rb
+++ cinemaddict-backend/app/serializers/review_serializer.rb
@@ -1,4 +1,4 @@
 class ReviewSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :body, :rating, :user, :movie
+  attributes :id, :title, :body, :rating, :user, :movie, :movie_id, :user_id
 end
