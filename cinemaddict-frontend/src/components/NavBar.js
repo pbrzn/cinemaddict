8a6c37284bf27cb03fa4ad34bf7a32fe5898e837
Patrick Brennan <pbrennanmusic@gmail.com>
2022-01-22 22:21:47 -0500

    fix: Refactored, now built around a conditional based around login status. Will now rerender onced logged in and logged out.

--- cinemaddict-frontend/src/components/NavBar.js
+++ cinemaddict-frontend/src/components/NavBar.js
@@ -1,38 +1,69 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import Logout from './users/Logout';
 import logo from '../images/Film-Reel-256.ico';
 
-const NavBar = () => {
-  let loggedIn = !!localStorage.getItem('jwt');
+class NavBar extends Component {
 
-  return (
-    <Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="/" className="brand">
-        <img
-        src={logo}
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-        alt=""
+  render() {
+    if (Object.keys(this.props.user).length > 0 || localStorage.jwt) {
+      return (
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/" className="brand">
+            <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
 
-        />{' '}
-      CINEMADDICT
-      </Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/movies" className="nav-link">Movies</Nav.Link>
-        <Nav.Link href="/reviews" className="nav-link">Reviews</Nav.Link>
-        {loggedIn ?
-          <Nav.Link href={`/profile/${JSON.parse(localStorage.getItem('user')).id}`} className="nav-link">
-            Profile
-          </Nav.Link> : <></>}
-      </Nav>
-      {loggedIn ? <Logout /> : <></> }
+            />{' '}
+          CINEMADDICT
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/movies" className="nav-link">Movies</Nav.Link>
+            <Nav.Link href="/reviews" className="nav-link">Reviews</Nav.Link>
+            <Nav.Link href="/profile" className="nav-link">
+              Profile
+            </Nav.Link>
+          </Nav>
+          <Logout />
 
-    </Container>
-  </Navbar>
-  )
+        </Container>
+      </Navbar>
+      )
+    } else {
+      return (
+        <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="/" className="brand">
+            <img
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt=""
+
+            />{' '}
+          CINEMADDICT
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/movies" className="nav-link">Movies</Nav.Link>
+            <Nav.Link href="/reviews" className="nav-link">Reviews</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      )
+    }
+  }
 }
 
-export default NavBar;
+const mapStateToProps = state => {
+  return {
+    user: state.users.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(NavBar);
